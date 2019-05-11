9023d48f8d7c0bffcd8a7da01d1b5a4b7ab482a8
apedaddy <ganesh.baral@hotmail.co.uk>
2019-05-11 18:57:39 +0100

    Two more ways to call function

--- import_entire_module/making-pizza.py
+++ import_entire_module/making-pizza.py
@@ -1,3 +1,20 @@
+
+'''
 import pizza
+
 pizza.make_pizza(16,'Tanduri hot')
 pizza.make_pizza(18,'Chicken Roast', 'Sweet corn', 'Black olive', 'Meatballs')
+
+'''
+# CALL DIFFERENTLY - Using as to give a function an alias
+'''
+from pizza import make_pizza as mp
+mp(16,'Tanduri hot')
+mp(18,'Chicken Roast', 'Sweet corn', 'Black olive', 'Meatballs')
+'''
+
+# CALL DIFFERENTLY - Using as to give a module an alias
+
+import pizza as p
+p.make_pizza(16,'Tanduri hot')
+p.make_pizza(18,'Chicken Roast', 'Sweet corn', 'Black olive', 'Meatballs')
