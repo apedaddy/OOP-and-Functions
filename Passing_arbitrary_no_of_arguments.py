4202140a8bd5c282edd48ca8b248a303f8460718
apedaddy <ganesh.baral@hotmail.co.uk>
2019-05-16 23:54:59 +0100

    input height and radius to calculate Cylinder

--- Passing_arbitrary_no_of_arguments.py
+++ Passing_arbitrary_no_of_arguments.py
@@ -1,14 +1,14 @@
 # Assume toppings in the pizza orders can be none or many
 # Hence arguments are not fixed, it can be any number of arguments
 
-def make_pizza (*toppings):
+def make_pizza (size, *toppings):
     ''' Print the list of toppings that were choosen'''
-    print ("\nFollowing toppings will be added")
+    print ("\nFollowing toppings will be added to".format(size))
     for topping in toppings:
         print ("- "+topping)
 
 # Single topping
-make_pizza('Pepperoni')
+make_pizza(16, 'Pepperoni')
 
 # multiple number of toppings
-make_pizza('mushroom', 'sweet corn', 'tanduri chicken')
+make_pizza(12, 'mushroom', 'sweet corn', 'tanduri chicken')
