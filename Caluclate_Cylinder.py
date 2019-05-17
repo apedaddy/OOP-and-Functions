ab8106e992b5c3847ed0334e924abc33c70f10b5
apedaddy <ganesh.baral@hotmail.co.uk>
2019-05-17 10:50:18 +0100

    Added

---
+++ Caluclate_Cylinder.py
@@ -0,0 +1,17 @@
+class Cylinder:
+
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+
+    def volume(self):
+        return self.height *3.14 *(self.radius)**2
+    def surface_area(self):
+        top = 3.14 * (self.radius**2)
+        return (2*top)+ (2*3.14*self.radius*self.height)
+height = int(input("Enter value for height: "))
+radius = int(input("Enter value for radius: "))
+
+calc_cylinder = Cylinder(height, radius)
+print("\nVolume of given cylinder is {}".format(calc_cylinder.volume()))
+print("Surface area of given cylinder is {}".format(calc_cylinder.surface_area()))
