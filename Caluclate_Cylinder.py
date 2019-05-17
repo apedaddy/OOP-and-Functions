0e6bcce594d901792dd755bdcd32ca88ed25cc82
apedaddy <ganesh.baral@hotmail.co.uk>
2019-05-17 10:51:50 +0100

    #coment added

--- Caluclate_Cylinder.py
+++ Caluclate_Cylinder.py
@@ -1,14 +1,16 @@
 class Cylinder:
-
+    # height and radius set to 1 as default value
     def __init__(self, height=1, radius=1):
         self.height = height
         self.radius = radius
 
     def volume(self):
         return self.height *3.14 *(self.radius)**2
+
     def surface_area(self):
         top = 3.14 * (self.radius**2)
         return (2*top)+ (2*3.14*self.radius*self.height)
+
 height = int(input("Enter value for height: "))
 radius = int(input("Enter value for radius: "))
 
