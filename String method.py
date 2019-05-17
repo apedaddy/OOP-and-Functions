939c5d6d6b6275ec49ed9d2b245be50692756087
apedaddy <ganesh.baral@hotmail.co.uk>
2019-05-17 21:20:59 +0100

    Spelling corrected

--- String method.py
+++ String method.py
@@ -2,7 +2,7 @@ class PrintString():
     def __init__(self):
         self.string = ""
     def get_string(self):
-        self.string = input("Etner your string: ")
+        self.string = input("Enter your string: ")
     def print_string(self):
         print ("\nThe string you entered is: ")
         print (self.string)
